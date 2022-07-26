a888b75431ea9527c4c43847f22f0220f86d93e2
b5 <sparkle_pony_2000@qri.io>
2022-07-26 12:02:02 -0400

    working on home page layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,25 @@ module.exports = {
   ],
  darkMode: 'class', // or 'media' or 'class'
  theme: {
-   extend: {},
+   extend: {
+    fontFamily: {
+      'space': ['Space Grotesk', 'sans-serif'], 
+      'space-mono': ['Space Mono', 'sans-serif'] 
+    },
+    colors: {
+     'n0-pink': {
+       100: '#FFF4F3',
+       200: '#FFE9E6',
+       300: '#FFD2CC',
+       400: '#FFBCB3',
+       500: '#FFAC9C',
+       600: '#E2847D',
+       700: '#CC6E66',
+       800: '#AF584F',
+       900: '#99463D',
+     },
+    },
+   },
  },
  variants: {
    extend: {},
