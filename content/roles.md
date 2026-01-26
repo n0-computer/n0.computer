8d62e0321241033c59c7470b9170d142abf0b253
b5 <sparkle_pony_2000@qri.io>
2026-01-26 11:43:01 -0500

    add biz dev role

---
+++ content/roles.md
@@ -0,0 +1,5 @@
++++
+title = "number 0: open roles"
+description = "Build on the edge with the team that lives there"
+template="roles.html"
++++
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,48 +1,49 @@
 module.exports = {
   content: [
-      './templates/**/*.html',
-      './static/js/**/*.js',
+    "./templates/**/*.html",
+    "./static/js/**/*.js",
+    "./content/**/*.md",
   ],
- darkMode: 'class', // or 'media' or 'class'
- theme: {
-   extend: {
-    fontFamily: {
-      'space': ['Space Grotesk', 'sans-serif'], 
-      'space-mono': ['Space Mono', 'sans-serif'] 
+  darkMode: "class", // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        space: ["Space Grotesk", "sans-serif"],
+        "space-mono": ["Space Mono", "sans-serif"],
+      },
+      colors: {
+        n0pink: {
+          100: "#FFF4F3",
+          200: "#FFE9E6",
+          300: "#FFD2CC",
+          400: "#FFBCB3",
+          500: "#FFAC9C",
+          600: "#E2847D",
+          700: "#CC6E66",
+          800: "#AF584F",
+          900: "#99463D",
+        },
+        n0gray: {
+          50: "#FAFAFA",
+          100: "#F4F4F5",
+          200: "#E4E4E7",
+          300: "#D4D4D8",
+          400: "#A1A1AA",
+          500: "#71717A",
+          600: "#52525B",
+          700: "#3F3F46",
+          800: "#27272A",
+          900: "#18181B",
+          1000: "#0E0E0F",
+        },
+        irohPurple: {
+          500: "#7C7CFF",
+        },
+      },
     },
-    colors: {
-     'n0pink': {
-        100: '#FFF4F3',
-        200: '#FFE9E6',
-        300: '#FFD2CC',
-        400: '#FFBCB3',
-        500: '#FFAC9C',
-        600: '#E2847D',
-        700: '#CC6E66',
-        800: '#AF584F',
-        900: '#99463D',
-     },
-     'n0gray': {
-        50 : "#FAFAFA",
-        100 : "#F4F4F5",
-        200 : "#E4E4E7",
-        300 : "#D4D4D8",
-        400 : "#A1A1AA",
-        500 : "#71717A",
-        600 : "#52525B",
-        700 : "#3F3F46",
-        800 : "#27272A",
-        900 : "#18181B",
-        1000 : "#0E0E0F",
-     },
-     'irohPurple': {
-        500: "#7C7CFF",
-     }
-    },
-   },
- },
- variants: {
-   extend: {},
- },
- plugins: [],
-}
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
